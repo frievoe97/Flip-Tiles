2d1d6898eede136240409e9756208db648a0a2e2
Friedrich Völkers <friedrich.voelkers@me.com>
2021-10-07 20:08:01 +0200

    Set Up Project

---
+++ Flip Tiles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flip Tiles
+//
+//  Created by Friedrich Kasper Völkers on 07.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
